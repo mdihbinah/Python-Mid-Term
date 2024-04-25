9aa3b08872ecb269c6a435617e623cfda979d867
mdihbinah <168050056+mdihbinah@users.noreply.github.com>
2024-04-25 08:24:05 +0600

    Add files via upload

---
+++ first.py
@@ -0,0 +1,31 @@
+print('kopa python kopa')
+print('go ahead')
+
+a=55
+b=6.4
+c=64
+print(a+b*c)
+age =45
+
+st='dhfjhkhf'
+st2="dhfhdh"
+is_single=True
+print(st,st2)
+
+print(type(a))
+print(type(b))
+print(type(st))
+
+# print('kopa python kopa')
+# print('go ahead')
+
+# ctrl + / : single line comment
+# shift +alt + A : multi line comment
+""" afnihfiuoihndkfhskdjhiererkhifnf 
+ doiheihffkhdskjhf 
+ kdjif
+"""
+
+print('kodom ali' '   ' 'kacha badam')
+text =f"kodom ali {age} living in {st} intersted with {st2}"
+print(text)
